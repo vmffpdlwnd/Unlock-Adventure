5cd9e613a5205daff86b6362478438ff40ac5d7b
vmffpdlwnd <leegunehee@gmail.com>
2025-02-14 11:38:01 +0900

    Feat: 코어 시스템 cs 추가
    creat GameManager.cs. InputSysytem.cs, SceneManager,cs
    
    게임 작동을 위한 매니저들 추가 및 작성

---
+++ Unlock Adventure/Core/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unlock_Adventure.Core
+{
+    public class GameManager
+    {
+        private SceneManager sceneManager;
+        private InputSystem inputSystem;
+
+        public GameManager(SceneManager sceneManager, InputSystem inputSystem)
+        {
+            this.sceneManager = sceneManager;
+            this.inputSystem = inputSystem;
+        }
+
+        public void Run()
+        {
+            // 게임 루프
+            while (true)
+            {
+                // 현재 씬 업데이트 (한 번만)
+                sceneManager.Update();
+
+                // 입력 처리
+                inputSystem.HandleInput();
+
+                // 화면 깜박임 방지를 위해 약간의 대기 시간 추가
+                System.Threading.Thread.Sleep(50);
+            }
+        }
+    }
+}
---
+++ Unlock Adventure/Core/InputSysystem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unlock_Adventure.Core
+{
+    public class InputSystem
+    {
+        SceneManager sceneManager;
+        public InputSystem(SceneManager sceneManager)
+        {
+            this.sceneManager = sceneManager;
+        }
+        public void HandleInput()
+        {
+            if (sceneManager.CurrentSceneType == SceneManager.SceneType.Intro)
+            {
+                if (Console.KeyAvailable)
+                {
+                    Console.ReadKey(true);
+                    sceneManager.ChangeScene(SceneManager.SceneType.FirstMap);
+                }
+            }
+        }
+    }
+}
