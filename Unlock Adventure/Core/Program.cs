97dfd2d204ee4ef5864d713547e2d63c9ab2a90b
vmffpdlwnd <leegunehee@gmail.com>
2025-02-14 11:38:53 +0900

    Feat: 메인 코드 폴더 이동 및 내용 작성
    feat Program.cs
    Core 폴더에 이동 및 내용 추가

---
+++ Unlock Adventure/Core/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unlock_Adventure.Core;
+
+namespace Unlock_Adventure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 각 매니저들 초기화
+            SceneManager sceneManager = new SceneManager();
+            InputSystem inputSystem = new InputSystem(sceneManager);
+            GameManager gameManager = new GameManager(sceneManager, inputSystem);
+
+            // 게임 시작
+            sceneManager.ChangeScene(SceneManager.SceneType.Intro);
+            gameManager.Run();
+        }
+    }
+}
