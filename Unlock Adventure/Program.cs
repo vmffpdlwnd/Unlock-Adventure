f4cd0d5fb288140f8c2a076e8f7de9507d994492
vmffpdlwnd <leegunehee@gmail.com>
2025-02-16 17:41:59 +0900

    Rename: Program 파일 이동
    rename Program.cs 이동

---
+++ Unlock Adventure/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnlockAdventure.Core;
+
+namespace UnlockAdventure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 콘솔 환경 설정
+            Console.Title = "Unlock Adventure";
+            Console.CursorVisible = false;
+
+            // 각 매니저들 초기화
+            var sceneManager = new Core.SceneManager();
+            var inputSystem = new Core.InputSystem(sceneManager);
+            var gameManager = new Core.GameManager(sceneManager, inputSystem);
+
+            try
+            {
+                // 게임 시작
+                sceneManager.ChangeScene(SceneManager.SceneType.Intro);
+                gameManager.Run();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error occurred: {ex.Message}");
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
